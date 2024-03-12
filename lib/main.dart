da473c6a7997dcee2b7d70810fa35944712f9145
Mussa <math_200901@hotmail.com>
2024-03-12 09:36:15 -0300

    Navigator flutter

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+
+import 'view/tela1_view.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => MainApp(),
+    ),
+  );
+}
+
+//
+// MainApp
+//
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Navegação',
+
+      //
+      //Rotas de navegação
+      //
+
+      initialRoute: 't1',
+      routes: {
+        't1': (context) => Tela1View(),
+      },
+    );
+  }
+}
